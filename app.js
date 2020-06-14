97c6cfb1117c49db7866bc356dcb7edfef00e4ec
Garrett Trott <garretttrott@gmail.com>
2020-06-13 18:02:50 -0700

    add index project info

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-var express = require("express");
+const express = require("express");
+const { projects } = require("./data.json");
 
-var app = express();
+const app = express();
 
+// set static folder
 app.use("/static", express.static("public"));
+// set pug as view engine
 app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index", { projects });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { projects });
+});
+
+app.get("/project");
 
 app.listen(3000, () => {
   console.log("the application is running on localhost:3000!");
